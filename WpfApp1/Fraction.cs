b1c3ff9d2dbac8895767035df164e9f789b9e28e
AntoneFranich <antone@franich.com>
2019-11-25 21:42:05 -0800

    Initial Commit
    Initial Commit

---
+++ WpfApp1/Fraction.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace FractionCalculator
+{
+    public class Fraction
+    {
+        //Define the numerator and denominator parameters as readonly.
+        private int numerator;
+        public int Numerator { get { return numerator; } }
+        private int denominator;
+        public int Denominator { get { return denominator; } }
+
+        //numerator and denominator need to be defined at creation.
+        public Fraction(int num, int dem)
+        {
+            numerator = num;
+            if (dem != 0)
+                denominator = dem;
+            else
+                throw new DenominatorZeroException("The Denominator Cannot be Zero!");
+            denominator = dem;
+        }
+
+
+
+
+        //Override the required operators, +,-,*,/ for our Fraction class.
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            Fraction ret = new Fraction((f1.Numerator * f2.Denominator) + (f2.Numerator * f1.Denominator), f1.Denominator * f2.Denominator);
+            ret.Simplify();
+            return ret;
+        }
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            Fraction ret = new Fraction((f1.Numerator * f2.Denominator) - (f2.Numerator * f1.Denominator), f1.Denominator * f2.Denominator);
+            ret.Simplify();
+            return ret;
+        }
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            Fraction ret = new Fraction(f1.Numerator * f2.Numerator, f1.Denominator * f2.Denominator);
+            ret.Simplify();
+            return ret;
+        }
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            Fraction ret = new Fraction(f1.Numerator * f2.Denominator, f1.Denominator * f2.Numerator);
+            ret.Simplify();
+            return ret;
+        }
+
+        //Just a method to simplify this fraction if we can.
+        public void Simplify()
+        {
+            int a = numerator;
+            int b = denominator;
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            int gdc = a;
+            if (a == 0)
+                gdc = b;
+
+            numerator /= gdc;
+            denominator /= gdc;
+
+        }
+
+        public static void AutomatedTest()
+        {
+            System.Random rand = new System.Random();
+
+            for(int i=0;i<100;i++)
+            {
+                Fraction first = new Fraction(rand.Next(-1000, 1000), rand.Next(-1000, 0000));
+                Fraction second = new Fraction(rand.Next(-1000, 1000), rand.Next(-1000, 0000));
+
+                int Operation = rand.Next(0, 100);
+                Fraction result = null;
+                string opString = "";
+                if (Operation >= 75)
+                {
+                    result = first + second;
+                    opString = "+";
+                }
+                else if (Operation >= 50)
+                {
+                    result = first - second;
+                    opString = "-";
+                }
+                else if (Operation >= 25)
+                {
+                    result = first * second;
+                    opString = "*";
+                }
+                else
+                {
+                    result = first / second;
+                    opString = "/";
+                }
+
+                Console.WriteLine("Test " + (i + 1) + ": first: " + first.Numerator+"/"+first.Denominator +" " +opString+" "+ second.Numerator+"/"+second.Denominator+" = "+ result.Numerator + "/" + result.Denominator);
+            }
+
+            //Test our exception as a last ditch.
+
+            try
+            {
+                Fraction ExceptionFraction = new Fraction(1, 0);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+    }
+
+    //Define a custom user exception for when the user attempts to enter 0 as a fraction denominator.
+    public class DenominatorZeroException : Exception
+    {
+        public DenominatorZeroException(string message)
+            : base(message)
+        {
+        }
+    }
+
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
+
+namespace FractionCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //Ensure we can only enter numbers in our text boxes. 
+        private static readonly Regex _regex = new Regex("[^0-9.-]+");
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+        private void NumberBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(((TextBox)sender).Text + e.Text);
+        }
+
+        //The value of one of the text boxes changed.
+        private void NumberBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Clear the results if there are any.
+            if (NumerResult.Text != "")
+                NumerResult.Text = "";
+            if (DenomResult.Text != "")
+                DenomResult.Text = "";
+
+            //I removed the check here as it felt bad to crash everytime the user enters 0 in a denominator.
+            //Check is done when the user attempts to trigger an operation instead.
+            //if(Int32.Parse(Denom1.Text) == 0 || Int32.Parse(Denom2.Text) == 0)
+            //	throw new DenominatorZeroException("The Denominator Cannot be Zero!");
+
+            //Check to see if we need to enable the Results button. Otherwise Disable the results button.
+            if (Numer1.Text != "" && Numer2.Text != "" && Denom1.Text != "" && Denom2.Text != "")
+                Results.IsEnabled = true;
+            else
+                Results.IsEnabled = false;
+        }
+
+        //We clicked on the results button. Create our fractions, perform the operation, and report the results.
+        private void Results_Click(object sender, RoutedEventArgs e)
+        {
+            Fraction first = new Fraction(Int32.Parse(Numer1.Text), Int32.Parse(Denom1.Text));
+            Fraction second = new Fraction(Int32.Parse(Numer2.Text), Int32.Parse(Denom2.Text));
+
+            Fraction result = null;
+            switch (Operator.SelectedIndex)
+            {
+                case 0:
+                    result = first + second;
+                    break;
+                case 1:
+                    result = first - second;
+                    break;
+                case 2:
+                    result = first * second;
+                    break;
+                case 3:
+                    result = first / second;
+                    break;
+
+            }
+            if (result == null)
+                throw new UnknownOperationException("The currently selected fraction operation is unknown!");
+
+            NumerResult.Text = result.Numerator.ToString();
+            DenomResult.Text = result.Denominator.ToString();
+        }
+    }
+
+    //We shouldn't ever throw this, but it's there in case we someone select something other than the 4 operators.
+    public class UnknownOperationException : Exception
+    {
+        public UnknownOperationException(string message)
+            : base(message)
+        {
+        }
+    }
+}
